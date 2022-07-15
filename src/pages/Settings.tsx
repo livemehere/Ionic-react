b3b8e8cbe222c16a5e13d632a3bda3a54491523b
livemehere <rhdxoals@gmail.com>
2022-07-16 02:40:19 +0900

    Alert + Modal + Actions Sheet API 사용 및 firebase PWA 배포

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,13 +1,26 @@
 import {
+  IonActionSheet,
+  IonButton,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
+  IonModal,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
+  useIonAlert,
+  useIonModal,
 } from "@ionic/react";
-import React from "react";
+import { caretForwardCircle, close, heart, share, trash } from "ionicons/icons";
+import React, { useState } from "react";
 
 const Settings: React.FC = () => {
+  const [presentAlert] = useIonAlert();
+  const [showModal, setShowModal] = useState(false);
+  const [showActionSheet, setShowActionSheet] = useState(false);
+
   return (
     <IonPage>
       <IonHeader>
@@ -21,7 +34,111 @@ const Settings: React.FC = () => {
             <IonTitle size={"large"}>SETTINGS</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonGrid>
+          <IonRow>
+            <IonCol size={"12"} className={"ion-text-center"}>
+              <IonButton
+                style={{ width: "200px" }}
+                onClick={() =>
+                  presentAlert({
+                    header: "Alert",
+                    subHeader: "Important message",
+                    message: "This is an alert!",
+                    buttons: [
+                      {
+                        text: "Cancel",
+                        role: "cancel",
+                        handler: () => {
+                          console.log("Alert canceled");
+                        },
+                      },
+                      {
+                        text: "OK",
+                        role: "confirm",
+                        handler: () => {
+                          console.log("Alert confirmed");
+                        },
+                      },
+                    ],
+                  })
+                }
+              >
+                Alert!
+              </IonButton>
+            </IonCol>
+            <IonCol size={"12"} className={"ion-text-center"}>
+              <IonButton
+                style={{ width: "200px" }}
+                onClick={() => setShowModal(true)}
+              >
+                Modal!
+              </IonButton>
+            </IonCol>
+            <IonCol size={"12"} className={"ion-text-center"}>
+              <IonButton
+                style={{ width: "200px" }}
+                onClick={() => setShowActionSheet(true)}
+              >
+                Show Actions Sheet!
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
+      <IonModal isOpen={showModal}>
+        <div>Hello!</div>
+        <IonButton onClick={() => setShowModal(false)}>닫기</IonButton>
+      </IonModal>
+      <IonActionSheet
+        isOpen={showActionSheet}
+        onDidDismiss={() => setShowActionSheet(false)}
+        cssClass="my-custom-class"
+        buttons={[
+          {
+            text: "Delete",
+            role: "destructive",
+            icon: trash,
+            id: "delete-button",
+            data: {
+              type: "delete",
+            },
+            handler: () => {
+              console.log("Delete clicked");
+            },
+          },
+          {
+            text: "Share",
+            icon: share,
+            data: 10,
+            handler: () => {
+              console.log("Share clicked");
+            },
+          },
+          {
+            text: "Play (open modal)",
+            icon: caretForwardCircle,
+            data: "Data value",
+            handler: () => {
+              console.log("Play clicked");
+            },
+          },
+          {
+            text: "Favorite",
+            icon: heart,
+            handler: () => {
+              console.log("Favorite clicked");
+            },
+          },
+          {
+            text: "Cancel",
+            icon: close,
+            role: "cancel",
+            handler: () => {
+              console.log("Cancel clicked");
+            },
+          },
+        ]}
+      />
     </IonPage>
   );
 };
